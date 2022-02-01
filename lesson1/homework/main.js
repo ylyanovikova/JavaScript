8b11e6133be13e45035bd9322d5ed64a5edd8e29
ylyanovikova <ylyanovikova@ukr.net>
2022-02-01 17:08:05 +0200

    js hw 1

---
+++ lesson1/homework/main.js
@@ -0,0 +1,185 @@
+// -Створити змінні.Присвоїти кожному з них значення: 'hello', 'owu', 'com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// let a = 'hello';
+// let b = 'owu';
+// let c = 'com';
+// let d = 'ua';
+// let e = 1;
+// let f = 10;
+// let g = -999;
+// let h = 123;
+// let i = 3.14;
+// let j = 2.7;
+// let k = 16;
+// let l = true;
+// let m = false;
+
+
+// Вивести кожну змінну за допомогою: console.log, alert, document.write -
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(f);
+// console.log(g);
+// console.log(h);
+// console.log(i);
+// console.log(j);
+// console.log(k);
+// console.log(l);
+// console.log(m);
+
+// alert(a);
+// alert(b);
+// alert(c);
+// alert(d);
+// alert(e);
+// alert(f);
+// alert(g);
+// alert(h);
+// alert(i);
+// alert(j);
+// alert(k);
+// alert(l);
+// alert(m);
+
+
+// document.write(`<p>${a}</p>`);
+// document.write(`<p>${b}</p>`);
+// document.write(`<p>${c}</p>`);
+// document.write(`<p>${d}</p>`);
+// document.write(`<p>${e}</p>`);
+// document.write(`<p>${f}</p>`);
+// document.write(`<p>${g}</p>`);
+// document.write(`<p>${h}</p>`);
+// document.write(`<p>${i}</p>`);
+// document.write(`<p>${j}</p>`);
+// document.write(`<p>${k}</p>`);
+// document.write(`<p>${l}</p>`);
+// document.write(`<p>${m}</p>`);
+
+
+
+//    Переприсвоїти кожній змінній з завдання значення на довільне.
+// a = 'name';
+// b = '10';
+// c = 5;
+// d = true;
+// e = 'some text';
+// f = 890;
+// g = false;
+// h = 90;
+// i = 76;
+// j = 'zero';
+// k = null;
+// l = 1;
+// m = 0;
+
+
+// Вивести кожну змінну за допомогою: console.log, alert, document.write -
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(f);
+// console.log(g);
+// console.log(h);
+// console.log(i);
+// console.log(j);
+// console.log(k);
+// console.log(l);
+// console.log(m);
+
+// alert(a);
+// alert(b);
+// alert(c);
+// alert(d);
+// alert(e);
+// alert(f);
+// alert(g);
+// alert(h);
+// alert(i);
+// alert(j);
+// alert(k);
+// alert(l);
+// alert(m);
+
+
+// document.write(`<p>${a}</p>`);
+// document.write(`<p>${b}</p>`);
+// document.write(`<p>${c}</p>`);
+// document.write(`<p>${d}</p>`);
+// document.write(`<p>${e}</p>`);
+// document.write(`<p>${f}</p>`);
+// document.write(`<p>${g}</p>`);
+// document.write(`<p>${h}</p>`);
+// document.write(`<p>${i}</p>`);
+// document.write(`<p>${j}</p>`);
+// document.write(`<p>${k}</p>`);
+// document.write(`<p>${l}</p>`);
+// document.write(`<p>${m}</p>`);
+
+
+
+
+
+//    Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ.З 'єднати їх в одну змінну person. (Не об'
+// єкт) -
+// let firstName = 'Yuliia';
+// let middleName = 'Osadchuk';
+// let lastName = 'Viktorivna';
+
+// let person = firstName + ' ' + middleName + ' ' + lastName;
+// console.log(person);
+
+
+
+// За допомогою 3 х різних prompt() отримати 3 слова які являються вашими Імям, По - Батькові та роками.
+// let name = prompt('Your name?');
+// let lastName = prompt('Your lastname?');
+// let age = prompt('Age?');
+
+// console.log(name + ' ' + lastName + ' ' + age);
+
+
+// // Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років". -
+// document.write(`<h1> Hello, ${name} ${lastName}. You are ${age} years old</h1>`);
+
+
+
+
+//    За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+// let a = 100;
+// let b = '100';
+// let c = true; -
+
+// console.log(typeof (a));
+// console.log(typeof (b));
+// console.log(typeof (c));
+
+
+
+
+// Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+// В однакових виразах не використовувати однакові оператори!!!
+// 5 < 6 - > true
+// 5 === 6 - > false
+// 5 > 6 - > false
+// 5 = 6 - > false
+// 10 = 10 - > true
+// 10 === 10 - > true
+// 10 != 10 - > false
+// 10 < 10 - > false
+// 10 > 10 - > false
+// 123 === '123' - > false
+// 123 = '123' - > true
+
+//    -
+//    Подумайте Какие значения выведет в окно браузера следующий фрагмент кода ? и почему ?
+// let str = "20";
+// let a = 5;
+// document.write(str + a + "<br/>"); //205, бо все стає стрінгою
+// document.write(str - a + "<br/>"); //15, віднімання  виконується
+// document.write(str * "2" + "<br/>"); // 40 виконується
+// document.write(str / 2 + "<br/>"); //10 виконується
