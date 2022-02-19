97103b98542056ae412f5750fb041e3a15679383
ylyanovikova <ylyanovikova@ukr.net>
2022-02-19 19:35:15 +0200

    add1 recursion

---
+++ lesson8/additional/js/main.js
@@ -0,0 +1,16 @@
+//написати рекурсивну функцію, яка збирає всі назви класів з файлу rules.html в окремий масив.масив вивести в консоль
+// let classArray = [];
+
+// function classList(start) {
+//    let children = start.children;
+//    for (let i = 0; i < children.length; i++) {
+//       let list = children[i].classList;
+//       for (const listElement of list) {
+//          classArray.push(listElement);
+//       }
+//       classList(children[i]);
+//    }
+// };
+
+// classList(document.body);
+// console.log(classArray);
