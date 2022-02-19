84282f6e8b2e9f294bdfcc96c87ff7f305384f25
ylyanovikova <ylyanovikova@ukr.net>
2022-02-19 13:41:29 +0200

    add2

--- lesson7/additional2/js/main.js
+++ lesson7/additional2/js/main.js
@@ -2,65 +2,117 @@
 // let user2 = userCardHolder('Viktoriia', 24, 2);
 // let user3 = userCardHolder('Iryna', 23, 3);
 
-function userCardHolder(id, balance = 100, transactionLimit = 100) {
-   let user = {
-      balance: balance,
-      transactionLimit: transactionLimit,
-      historyLogs: [],
-      key: id
-   };
+// function userCardHolder() {
+//    let users = [{
+//          key: 1,
+//          balance: 100,
+//          transactionLimit: 100,
+//          historyLogs: [],
+//       },
+//       {
+//          key: 2,
+//          balance: 100,
+//          transactionLimit: 100,
+//          historyLogs: [],
+//       },
+//       {
+//          key: 3,
+//          balance: 100,
+//          transactionLimit: 100,
+//          historyLogs: [],
+//       }
+//    ];
 
-   let usersArr = [];
-   let user1 = userCardHolder(1);
-   let user2 = userCardHolder(2);
-   let user3 = userCardHolder(3);
 
-   usersArr.push(user1, user2, user3);
-   console.log(usersArr);
+//    return {
+//       userCard: (key) => {
+//          if (typeof key === 'number') {
+//             console.log(userCardHolder())
+//          } else {
+//             console.log('I need a number');
+//          }
+//       },
+//       getCardOptions: (key) => {
+//          users.find(user => {
+//             if (user.key === key) {
+//                console.log(user);
+//             }
+//          })
+//       },
+//       putCredits: (num, key) => {
+//          users.find(user => {
+//             if (user.key === key) {
+//                user.historyLogs.push({
+//                   operationType: 'PutCredits',
+//                   credits: num,
+//                   operationTime: new Date().toDateString()
+//                });
+//                return user.balance += num;
+//             }
+//          })
 
+//       },
+//       takeCredits: (num, key) => {
+//          users.find(user => {
+//             if (user.key === key) {
+//                if (user.balance >= num && user.transactionLimit >= num) {
+//                   user.historyLogs.push({
+//                      operationType: 'TakeCredits',
+//                      credits: num,
+//                      operationTime: new Date().toDateString()
+//                   });
+//                   return user.balance -= num;
+//                } else {
+//                   console.log('Not enough of money or increase the limit');
 
+//                }
+//             }
+//          })
+//       },
+//       setTransactionLimit: (num, key) => {
+//          users.find(user => {
+//             if (key === user.key) {
+//                if (typeof num === 'number') {
+//                   user.historyLogs.push({
+//                      operationType: 'ChangeTransactionLimit',
+//                      credits: num,
+//                      operationTime: new Date().toDateString()
+//                   });
+//                   return user.transactionLimit = num;
+//                } else {
+//                   console.log('Enter new limit number');
+//                }
+//             }
+//          })
+//       },
+//       transferCredits: (key, num, card) => {
+//          users.find(user => {
+//             if (user.key === key && user.balance >= num && user.transactionLimit >= num) {
+//                return user.balance -= num;
+//             }
+//          });
+//          users.find(user => {
+//             if (user.key === card) {
+//                return user.balance += num;
+//             }
+//          })
+//       }
 
-   return {
-      userCard: (key) => {
-         if (typeof key === 'number') {
-            console.log(userCardHolder())
-         } else {
-            console.log('I need a number');
-         }
-      },
-      getInfo: (key) => {
-         user['key'] = key;
-         return user;
-      },
-      putCredits: (num, key) => {
-         console.log(user[key]);
-
-         if (key === user.key) {
-            return balance += num;
-
-         }
-
-      },
-   }
-};
-// takeCredits: (num) => {
-//    if (transactionLimit < num && balance < num) {
-//       console.error('Not enough of money');
-//    } else {
-//       return balance - num;
 //    }
-// }
-
-
-
-
-
-
-//userCardHolder().userCard(2);
-
-// console.log(userCardHolder().getInfo(3));
-// console.log(userCardHolder().getInfo(1));
-// console.log(userCardHolder().getInfo(2));
-// console.log(userCardHolder().putCredits(300, 2));
-// console.log(userCardHolder().getInfo(2));
-userCardHolder();
+// };
+// let users = userCardHolder();
+// users.userCard(4);
+// users.getCardOptions(2);
+// users.putCredits(200, 1);
+// users.getCardOptions(1);
+// users.putCredits(400, 3);
+// users.getCardOptions(3);
+// users.takeCredits(300, 3);
+// users.getCardOptions(3);
+// users.takeCredits(50, 3);
+// users.getCardOptions(3);
+// users.setTransactionLimit(400, 3);
+// users.getCardOptions(3);
+// users.transferCredits(3, 100, 2);
+// users.getCardOptions(3);
+// users.getCardOptions(2);
